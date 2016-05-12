b3f1d10526f132f2ae3aa44afde7f239530840ae
Jose Romero <spookmx@gmail.com>
2016-05-12 18:39:25 -0500

    Gstreamer console video requests

--- client/signages/signage-gstreamer-detail.controller.ng.js
+++ client/signages/signage-gstreamer-detail.controller.ng.js
@@ -233,9 +233,9 @@ angular.module('digitalsignageApp')
   $scope.productionURL = "http://tx11wfm01c.cloudapp.net/";
   $scope.$watch("video", function() {
     if($scope.video){
-      gs.send(JSON.stringify({"link": $scope.productionURL+$scope.video.url(),"width": "1280","height": "720","x": "83","y": "44"}));
       console.log("New video requested");
       console.log(JSON.stringify({"link": $scope.productionURL+$scope.video.url(),"width": "1280","height": "720","x": "83","y": "44"}));
+      gs.send(JSON.stringify({"link": $scope.productionURL+$scope.video.url(),"width": "1280","height": "720","x": "83","y": "44"}));
     }
   });
 
